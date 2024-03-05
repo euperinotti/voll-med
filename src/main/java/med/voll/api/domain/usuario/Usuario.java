12309cb6e4f15fac5853b8d7f04355f1ee519a2c
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-03-04 22:36:57 -0300

    feat: add user entity and user migration

---
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,26 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+public class Usuario {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String login;
+  private String senha;
+}
