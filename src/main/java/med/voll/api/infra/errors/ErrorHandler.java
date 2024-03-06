8d071fa720a67ac8543a2a1e6fa6eebf0577ba28
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-03-06 17:11:33 -0300

    feat: add error handling methods

--- src/main/java/med/voll/api/infra/errors/ErrorHandler.java
+++ src/main/java/med/voll/api/infra/errors/ErrorHandler.java
@@ -2,7 +2,11 @@ package med.voll.api.infra.errors;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,4 +30,24 @@ public class ErrorHandler {
 
     return ResponseEntity.badRequest().body(errorsList.stream().map(DadosErroValidacao::new).toList());
   }
+
+  @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Object> handleBadCredentialsError() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<Object> handleAuthenticationError() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Object> handleAccessoDeniedError() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handle500Error(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " +ex.getLocalizedMessage());
+    }
 }
