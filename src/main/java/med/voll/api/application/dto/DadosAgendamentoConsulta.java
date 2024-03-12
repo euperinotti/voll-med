6580bee9ece004c7c19a702d60e7d8bfe9c3bd26
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-03-11 22:34:51 -0300

    feat: refactor consulta dto

--- src/main/java/med/voll/api/application/dto/DadosAgendamentoConsulta.java
+++ src/main/java/med/voll/api/application/dto/DadosAgendamentoConsulta.java
@@ -4,12 +4,16 @@ import java.time.LocalDateTime;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.medico.Especialidade;
 
 public record DadosAgendamentoConsulta(
     Long idMedico,
 
     @NotNull Long idPaciente,
 
-    @NotNull @Future LocalDateTime data) {
+    @NotNull @Future LocalDateTime data,
+    
+    Especialidade especialidade
+    ) {
 
 }
