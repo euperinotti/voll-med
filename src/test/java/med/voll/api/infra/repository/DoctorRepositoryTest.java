10bb5dff2c483c705f705efeb3080b9a539eb14a
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-03-24 17:59:10 -0300

    test: doctor repository

--- src/test/java/med/voll/api/infra/repository/DoctorRepositoryTest.java
+++ src/test/java/med/voll/api/infra/repository/DoctorRepositoryTest.java
@@ -1,5 +1,6 @@
 package med.voll.api.infra.repository;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.time.DayOfWeek;
@@ -9,17 +10,18 @@ import java.time.temporal.TemporalAdjusters;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import med.voll.api.domain.address.Address;
 import med.voll.api.domain.doctor.Doctor;
 import med.voll.api.domain.doctor.Specialty;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
-// @ActiveProfiles("test")
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@ActiveProfiles("test")
 public class DoctorRepositoryTest {
 
   @Autowired
@@ -32,5 +34,20 @@ public class DoctorRepositoryTest {
     Doctor doctor = repository.findRandomMedicAvailableOnDate(Specialty.CARDIOLOGIA, nextMonday);
 
     assertNull(doctor);
+  }
+
+  @Test
+  void shouldReturnADoctorIfIsAvailable() {
+
+    Address mockAddress = new Address("Gales Glen", "Santa Cruz", "86609-020", "1329", "house", "Cascavel", "Parana");
+    Doctor newDoctor = new Doctor(null, "Juan Daniels", "razmig@gutre.so", "(306) 391-6443", "000000", Specialty.CARDIOLOGIA, mockAddress, true);
+
+    repository.save(newDoctor);
+
+    LocalDateTime nextMonday = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY)).atTime(10, 0);
+
+    Doctor doctor = repository.findRandomMedicAvailableOnDate(Specialty.CARDIOLOGIA, nextMonday);
+
+    assertNotNull(doctor);
   }
 }
