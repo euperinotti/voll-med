bdf05c9e6316dbd10313b86c234b4f0fc305da74
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-08 11:58:19 -0300

    refactor: domain exceptions

--- src/main/java/med/voll/api/application/error/InvalidParamException.java
+++ src/main/java/med/voll/api/application/error/InvalidParamException.java
@@ -1,6 +1,6 @@
 package med.voll.api.application.error;
 
-public class InvalidParamException extends Throwable {
+public class InvalidParamException extends RuntimeException {
   public InvalidParamException(String message) {
     super(message);
   }
